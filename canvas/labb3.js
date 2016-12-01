4a4704e7a116293a11290d9ea09b6773b851fc15
Petri Jahnsson <meikyo72@gmail.com>
2016-12-01 20:45:27 +0100

    Initial commit

---
+++ canvas/labb3.js
@@ -0,0 +1,282 @@
+﻿
+(function(){
+	var canvas=document.getElementById("can");
+	var context=canvas.getContext("2d");
+	let button=document.getElementById("rita");
+	button.onclick=rita;
+	let button2=document.getElementById("rensa");
+	button2.onclick=rensa;
+	var statusB=document.getElementById('statusB');
+	let expbutton=document.getElementById('exp');
+	expbutton.onclick=expImg;
+	
+
+
+function rita(){
+if (document.getElementById('ranrectangle').checked){
+	for (let rectangle=0; rectangle<20; rectangle++){
+		drawRectangle(canvas, context);
+	}statusB.innerHTML='Ritar random rektanglar ';
+} else if (document.getElementById('rancircle').checked){
+	for (let circle=0; circle<20; circle++){
+		drawCircle(canvas, context);
+	}statusB.innerHTML='Ritar random cirklar ';
+} else if (document.getElementById('rantriangle').checked){
+	for (let triangle=0; triangle<2; triangle++){
+		drawTriangle(canvas, context);
+	}statusB.innerHTML='Ritar random trianglar ';
+}else if (document.getElementById('polygon').checked){
+	drawPolygon(canvas,context);
+}else if (document.getElementById('rectangle').checked){
+	drawRec(canvas, context);
+}else if (document.getElementById('circle').checked){
+	drawCir(canvas, context);
+}else if (document.getElementById('triangle').checked){
+	drawTri(canvas, context);
+}	
+}
+
+function drawRectangle(canvas, context){
+	let selectObj=document.getElementById('sel');
+	let index=selectObj.selectedIndex;
+	let color=selectObj[index].value;
+	if (color=='black'){
+		document.getElementById('farg').value='#000000';
+	}else if (color=='blue') {
+		document.getElementById('farg').value='#0000FF';
+	}else if (color=='green'){
+		document.getElementById('farg').value='#008000';
+	}
+	let hcol=document.getElementById('farg').value;
+	let w=Math.floor(Math.random()*40);
+	let x=Math.floor(Math.random()*canvas.width);
+	let y=Math.floor(Math.random()*canvas.height);
+	context.fillStyle=hcol;
+	context.fillRect(x, y, w, w);
+}
+function drawRec(canvas, context){
+	statusB.innerHTML='Ritar rektangel Markera två punkter ';
+	let selectObj=document.getElementById('sel');
+	let index=selectObj.selectedIndex;
+	let color=selectObj[index].value;
+	if (color=='black'){
+		document.getElementById('farg').value='#000000';
+	}else if (color=='blue') {
+		document.getElementById('farg').value='#0000FF';
+	}else if (color=='green'){
+		document.getElementById('farg').value='#008000';
+	}
+	let hcol=document.getElementById('farg').value;
+	let c=0;
+	let mx=0;
+	let my=0;
+	context.beginPath();
+	canvas.addEventListener('click', recFunc);
+	function recFunc(event){
+	c=c+1;
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;	
+	if (c==1){
+		mx=x;
+		my=y;
+		
+	}else {
+		x=x-mx;
+		y=y-my;
+		context.fillStyle=hcol;
+		context.fillRect(mx,my,x,y);
+		c=0;
+		canvas.removeEventListener('click', recFunc);
+	}
+	}	
+}
+function drawCircle(canvas, context){
+	let selectObj=document.getElementById('sel');
+	let index=selectObj.selectedIndex;
+	let color=selectObj[index].value;
+	if (color=='black'){
+		document.getElementById('farg').value='#000000';
+	}else if (color=='blue') {
+		document.getElementById('farg').value='#0000FF';
+	}else if (color=='green'){
+		document.getElementById('farg').value='#008000';
+	}
+	let hcol=document.getElementById('farg').value;
+	let radius=Math.floor(Math.random()*40);
+	let x=Math.floor(Math.random()*canvas.width);
+	let y=Math.floor(Math.random()*canvas.height);
+	context.beginPath();
+	context.arc(x, y, radius, 0, 2*Math.PI, true);
+	context.fillStyle=hcol;
+	context.fill();
+}
+function drawCir(canvas, context){
+	statusB.innerHTML='Ritar cirkel Markera två punkter ';
+	let selectObj=document.getElementById('sel');
+	let index=selectObj.selectedIndex;
+	let color=selectObj[index].value;
+	if (color=='black'){
+		document.getElementById('farg').value='#000000';
+	}else if (color=='blue') {
+		document.getElementById('farg').value='#0000FF';
+	}else if (color=='green'){
+		document.getElementById('farg').value='#008000';
+	}
+	let hcol=document.getElementById('farg').value;
+	let c=0;
+	let mx=0;
+	let my=0;
+	let r=0;
+	context.beginPath();
+	canvas.addEventListener('click', cirFunc);
+	function cirFunc(event){
+	c=c+1;
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;	
+	if (c==1){
+		mx=x;
+		my=y;
+		
+	}else {
+		r=x-mx;
+		if (r<0){
+			r=-1*r;
+		}
+		context.arc(x, y, r, 0, 2*Math.PI, true);
+		context.fillStyle=hcol;
+		context.fill();		
+		c=0;
+		canvas.removeEventListener('click',cirFunc);
+	}
+	}
+}
+function drawTriangle(canvas, context){
+	let selectObj=document.getElementById('sel');
+	let index=selectObj.selectedIndex;
+	let color=selectObj[index].value;
+	if (color=='black'){
+		document.getElementById('farg').value='#000000';
+	}else if (color=='blue') {
+		document.getElementById('farg').value='#0000FF';
+	}else if (color=='green'){
+		document.getElementById('farg').value='#008000';
+	}
+	let hcol=document.getElementById('farg').value;
+	let w=Math.floor(Math.random()*canvas.width);
+	let w2=Math.floor(Math.random()*canvas.height);
+	let w3=Math.floor(Math.random()*canvas.width);
+	let w4=Math.floor(Math.random()*canvas.height);
+	let x=Math.floor(Math.random()*canvas.width);
+	let y=Math.floor(Math.random()*canvas.height);
+	context.beginPath();
+	context.moveTo(x, y);
+	context.lineTo(w, w2);
+	context.lineTo(w3, w4);
+	context.closePath();
+	context.fillStyle=hcol;
+	context.fill();
+}
+function drawTri(canvas, context){
+	statusB.innerHTML='Ritar triangel Markera tre punkter ';
+	let selectObj=document.getElementById('sel');
+	let index=selectObj.selectedIndex;
+	let color=selectObj[index].value;
+	if (color=='black'){
+		document.getElementById('farg').value='#000000';
+	}else if (color=='blue') {
+		document.getElementById('farg').value='#0000FF';
+	}else if (color=='green'){
+		document.getElementById('farg').value='#008000';
+	}
+	let hcol=document.getElementById('farg').value;
+	let c=0;
+	context.beginPath();
+	canvas.addEventListener('click', triFunc);
+	function triFunc(event){
+	c=c+1;
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;	
+	if (c==1){
+		let mx=x;
+		let my=y;
+		context.moveTo(mx,my);		
+	}else {		
+		context.lineTo(x,y);
+		context.stroke();
+		
+		if (c<3){
+			/*let statusB=document.getElementById('statusB');*/
+			statusB.innerHTML='Fler punkter krävs för en triangel';
+		}else if(c==3){
+		context.closePath();
+		context.fillStyle=hcol;
+		context.fill();
+		c=0;
+		statusB.innerHTML=' ';
+		canvas.removeEventListener('click', triFunc);
+		}		
+	}
+	}	
+}
+function drawPolygon(canvas, context){
+	let selectObj=document.getElementById('sel');
+	let index=selectObj.selectedIndex;
+	let color=selectObj[index].value;
+	if (color=='black'){
+		document.getElementById('farg').value='#000000';
+	}else if (color=='blue') {
+		document.getElementById('farg').value='#0000FF';
+	}else if (color=='green'){
+		document.getElementById('farg').value='#008000';
+	}
+	let hcol=document.getElementById('farg').value;
+	let c=0;
+	context.beginPath();
+	canvas.addEventListener('click', polFunc);
+	function polFunc(event){
+	c=c+1;
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;	
+	if (c==1){
+		let mx=x;
+		let my=y;
+		context.moveTo(mx,my);		
+	}else {		
+		context.lineTo(x,y);
+		context.stroke();
+		statusB.innerHTML='Ritar polygon Markera minst 5 punkter och avsluta med Rita Polygon ';
+		let button3=document.getElementById("avbryt");
+		button3.onclick=avsluta;
+		function avsluta(){	
+		if (c<5){
+			statusB.innerHTML='Fler punkter krävs för en polygon';
+		}else{
+		context.closePath();
+		context.fillStyle=hcol;
+		context.fill();
+		c=0;
+		canvas.removeEventListener('click', polFunc);
+		}
+	}	
+	}
+	}
+	
+}
+function expImg(){
+	window.open(document.getElementById('context').toDataURL("image/png"));
+}
+
+function rensa(){
+	let canvas=document.getElementById("can");
+	let context=canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	
+}
+
+})();
+
---
+++ javascr/script1.js
@@ -0,0 +1,48 @@
+﻿/*var ettNummer = Number(prompt("ange ett nummer mellan 1 och 10", ""));
+if (ettNummer<11 && ettNummer>0)
+	alert ("Grattis");
+else
+	alert ("Fel, fel, fel");*/
+
+
+
+/*var x = 10; 
+var rand = Math.floor(Math.random()*x) + 1;
+var ettNummer = Number(prompt("ange ett nummer mellan 1 och 10", ""));
+if (ettNummer==rand)
+	alert ("Grattis");
+else
+	alert ("Fel, fel, fel. Rätt svar är " + rand);*/
+
+/*var ettNummer = Number(prompt("ange ett nummer mellan 1 och 3", ""));
+switch (ettNummer){
+	case 1: console.log ("Du tryckte på 1");
+	break;
+	case 2: console.log ("Du tryckte på 2");
+	break;
+	case 3: console.log ("Du tryckte på 3");
+	break;
+}*/
+
+/*var y="#";
+for (var x=7; x>0; x--){
+	console.log (y);
+	y=y+"#";
+}*/
+
+var y, z;
+for (var x=1; x<101; x++){
+	y=x%3;
+	z=x%5;
+	if (y===0 && z===0)
+		console.log ("FizzBuzz");
+	else if (y===0)
+		console.log ("Fizz");
+	else if (z===0)
+		console.log ("Buzz");
+	else
+		console.log (x);
+	
+}
+
+
