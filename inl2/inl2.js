6946f6349d5a9e1a35f68eb1ad7e836b47b2d99e
Petri Jahnsson <meikyo72@gmail.com>
2017-03-27 19:33:50 +0200

    local2

--- inl2/inl2.js
+++ inl2/inl2.js
@@ -3,7 +3,7 @@ window.onload = function () {
     let glMig = document.getElementById('glMig');
     let tableBody = document.getElementById('tableBody');
     let addText = document.getElementById('addText');
-    addName.innerHTML = localStorage.getItem("petriBaseN");
+    addName.innerHTML = localStorage.petriBaseN;
     
     addText.addEventListener('keypress', function(event) {
         if (event.keyCode==13){
@@ -25,7 +25,7 @@ window.onload = function () {
 				tr.innerHTML = `<td>${data.tid}</td> <td>${data.id}</td> <td>${data.namn}</td> <td>${data.med}</td>`;
 				tableBody.appendChild(tr);
 			}
-    localStorage.setItem("petriBaseN", addName.value);
+    localStorage.petriBaseN = addName.value;
     glMig.addEventListener('click', function(event){
         localStorage.removeItem("petriBaseN");
     });
