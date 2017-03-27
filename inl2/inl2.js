97966d63705954b81f42053ea9bdc462b1361a54
Petri Jahnsson <meikyo72@gmail.com>
2017-03-27 19:27:23 +0200

    localstorage

--- inl2/inl2.js
+++ inl2/inl2.js
@@ -3,7 +3,7 @@ window.onload = function () {
     let glMig = document.getElementById('glMig');
     let tableBody = document.getElementById('tableBody');
     let addText = document.getElementById('addText');
-    
+    addName.innerHTML = localStorage.getItem("petriBaseN");
     
     addText.addEventListener('keypress', function(event) {
         if (event.keyCode==13){
@@ -25,4 +25,8 @@ window.onload = function () {
 				tr.innerHTML = `<td>${data.tid}</td> <td>${data.id}</td> <td>${data.namn}</td> <td>${data.med}</td>`;
 				tableBody.appendChild(tr);
 			}
+    localStorage.setItem("petriBaseN", addName.value);
+    glMig.addEventListener('click', function(event){
+        localStorage.removeItem("petriBaseN");
+    });
 }
