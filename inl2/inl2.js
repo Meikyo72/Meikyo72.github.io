d86c0658c0a854c27d1751c2f5044dd22ffa6df9
Petri Jahnsson <meikyo72@gmail.com>
2017-03-27 18:04:33 +0200

    major uppload

---
+++ inl2/inl2.js
@@ -0,0 +1,28 @@
+window.onload = function () {
+    let addName = document.getElementById('addName');
+    let glMig = document.getElementById('glMig');
+    let tableBody = document.getElementById('tableBody');
+    let addText = document.getElementById('addText');
+    
+    
+    addText.addEventListener('keypress', function(event) {
+        if (event.keyCode==13){
+            let datum = new Date();
+				firebase.database().ref('mess/').push({
+					tid: datum,
+					id: datum.getTime(),
+					namn: addName.value,
+					med: addText.value
+				});
+            }
+			});
+    firebase.database().ref('mess/').on('child_added', function(snapshot, prevChildKey) {
+				let data = snapshot.val();
+				addMessToTable(data);
+			});
+    function addMessToTable(data) {
+				let tr = document.createElement('tr');
+				tr.innerHTML = `<td>${data.tid}</td> <td>${data.id}</td> <td>${data.namn}</td> <td${data.med}></td>`;
+				tableBody.appendChild(tr);
+			}
+}
