df73fc468be7e50b10c6f2f44a8a99cff6d33967
Petri Jahnsson <meikyo72@gmail.com>
2017-03-30 09:08:11 +0200

    Auth 1

--- inl2/inl2.js
+++ inl2/inl2.js
@@ -4,6 +4,24 @@ window.onload = function () {
     let tableBody = document.getElementById('tableBody');
     let addText = document.getElementById('addText');
     let scrollRow = document.getElementById('scrollRow');
+    var provider = new firebase.auth.GithubAuthProvider();
+    
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
     
     if (localStorage.petriBaseN==undefined){
         addName.value='';
