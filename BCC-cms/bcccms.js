ed0ea6b3a17a7451e6baba28828b1cf0e00f2473
Petri Jahnsson <meikyo72@gmail.com>
2018-02-10 18:26:41 +0100

    Add files via upload

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -132,7 +132,7 @@ window.onload = function () {
   // ...
 });
         function addMessToTable(data) {
-        console.log(idNr.$key);
+        console.log(data.idNr.$key);
 				let tr = document.createElement('tr');
 				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td>`;
 				tableBody.appendChild(tr);
