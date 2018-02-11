5afc5fbc0960891dd79b0badc4d8b5067c77bdef
Petri Jahnsson <meikyo72@gmail.com>
2018-02-11 17:18:57 +0100

    Add files via upload

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -131,7 +131,7 @@ window.onload = function () {
 });
         function addMessToTable(data, dbObjectKey) {
 				let tr = document.createElement('tr');
-				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td> <td><button type="button" class="btn btn-info btn-sm" id="delete" onclick="console.log(${bcc.key})">${`Ta Bort`}</button></td>`;
+				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td> <td><button type="button" class="btn btn-info btn-sm" id="delete">${`Ta Bort`}</button></td>`;
 				tableBody.appendChild(tr);
                 addInternalID.value='';
                 addIDnr.value='';
@@ -150,6 +150,9 @@ window.onload = function () {
 					});
 				})
 			}
+      document.getElementById('delete').addEventListener('click', function(event){
+        firebase.database().ref('bcc/' + key).remove();
+      })
 			sortFunc(sortInternalID, 'internalID');
 			sortFunc(sortIDnr, 'idNr');
 			sortFunc(sortAdress, 'adress');
