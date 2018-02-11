8e7ee5664728e62e0afc4fe7fa1a89c8e37dca76
Petri Jahnsson <meikyo72@gmail.com>
2018-02-11 14:32:08 +0100

    Add files via upload

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -138,7 +138,7 @@ window.onload = function () {
                 addIDnr.value='';
                 addAdress.value='';
                 addCity.value='';
-                dbObjectKey.value='';
+                dbObjectKey='';
 			}
         function sortFunc(sortbutt, sortKey) {
 				sortbutt.addEventListener('click', function(event) {
