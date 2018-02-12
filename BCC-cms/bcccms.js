3be6edb5e9cd9c75cbb6993fd83b8ef3c5ed8dcc
Petri Jahnsson <meikyo72@gmail.com>
2018-02-12 12:49:53 +0100

    Add files via upload

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -54,7 +54,6 @@ window.onload = function () {
   let addStorage = document.getElementById('addStorage');
   let addParking = document.getElementById('addParking');
   let addPool = document.getElementById('addPool');
-  //let nDelete = document.getElementById('delete');
 
     loggaIn.addEventListener('click', function(event){
     firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -133,17 +132,15 @@ window.onload = function () {
         function addMessToTable(data) {
         let remKey = data.objectKey;
 				let tr = document.createElement('tr');
-				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td> <td><button type="button" class="btn btn-info btn-sm" id="${remKey}" value="${remKey}">Delete ${remKey}</button></td>`;
+				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td> <td>${data.objectKey}</td>`;
         console.log('Button ID ');
 				tableBody.appendChild(tr);
                 addInternalID.value='';
                 addIDnr.value='';
                 addAdress.value='';
                 addCity.value='';
+                dbObjectKey.value='';
 			}
-      /*document.getElementsByClassName('delete').addEventListener('click', function(event){
-        console.log('Rowkey '+button.value);
-      })*/
         function sortFunc(sortbutt, sortKey) {
 				sortbutt.addEventListener('click', function(event) {
 					tableBody.innerHTML = '';
