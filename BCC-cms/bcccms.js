2028adca08e8890fb2c1373f930f7c885801225a
Petri Jahnsson <meikyo72@gmail.com>
2018-02-12 12:23:57 +0100

    Add files via upload

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -133,16 +133,16 @@ window.onload = function () {
         function addMessToTable(data) {
         let remKey = data.objectKey;
 				let tr = document.createElement('tr');
-				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td> <td><button type="button" class="btn btn-info btn-sm" onClick=deleteRow("+String(remKey)+")>Delete ${remKey}</button></td>`;
+				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td> <td><button type="button" class="btn btn-info btn-sm" value=remKey id="delete">Delete ${remKey}</button></td>`;
 				tableBody.appendChild(tr);
                 addInternalID.value='';
                 addIDnr.value='';
                 addAdress.value='';
                 addCity.value='';
 			}
-      function deleteRow(rowKey) {
-        console.log(rowKey);
-      }
+      nDelete.addEventListener('click', function(event){
+        console.log('Rowkey '+button.value);
+      })
         function sortFunc(sortbutt, sortKey) {
 				sortbutt.addEventListener('click', function(event) {
 					tableBody.innerHTML = '';
