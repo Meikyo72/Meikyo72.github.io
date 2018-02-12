91bfa13ba334ec1bb592e52ffb46e1d96fd6caa5
Petri Jahnsson <meikyo72@gmail.com>
2018-02-12 10:47:23 +0100

    Add files via upload

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -132,6 +132,7 @@ window.onload = function () {
 });
         function addMessToTable(data) {
         let remKey = data.objectKey.toString();
+        console.log(remKey);
 				let tr = document.createElement('tr');
 				tr.innerHTML = `<td>${data.internalID}</td> <td>${data.idNr}</td> <td>${data.adress}</td> <td>${data.city}</td> <td><button type="button" class="btn btn-info btn-sm" id="delete" onclick="firebase.database().ref('bcc/' + ${remKey}).remove();">${remKey}</button></td>`;
 				tableBody.appendChild(tr);
