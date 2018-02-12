c7c4df67fade9ce05e9c06413116d4032cff4e10
Petri Jahnsson <meikyo72@gmail.com>
2018-02-12 23:21:10 +0100

    Add files via upload

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -117,6 +117,7 @@ window.onload = function () {
     firebase.database().ref('bcc/').on('child_added', function(snapshot, prevChildKey) {
 				let data = snapshot.val();
 				addMessToTable(data);
+        document.getElementsByClassName("form-group").reset();
 			});
 
       }).catch(function(error) {
