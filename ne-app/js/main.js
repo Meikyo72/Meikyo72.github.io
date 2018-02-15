ac85df4823b859479377fa72dd241e9f4719ee8e
Petri Jahnsson <meikyo72@gmail.com>
2018-02-15 17:49:03 +0100

    Add files via upload

---
+++ ne-app/js/main.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+/*let objectPrensentation = document.getElementById('object-presentation');
+let modalDiv = document.getElementById('modalDiv');
+let db = firebase.database();
+let bccRef = db.ref('bcc/');
+
+bccRef.on("child_added", function(snap, prevChildKey) {
+  //console.log(snap.val());
+  makeDivFunc(snap.val());
+  snap.forEach(function(childSnapshot) {
+    let key = childSnapshot.key;
+    let childData = childSnapshot.val();
+    //console.log(key +' '+ childData);
+  });
+});*/
+class RealObject extends Ract.Component {
+  render() {
+    return (
+      <div>
+        <p>Testar lite</p>
+      </div>
+    )
+  }
+}
+ReactDOM.render (
+  <div>
+    <RealObject />
+  </div>
+  document.getElementById('object-prensentation')
+);
+}
