e8e4ea54d3e27c2336c7e15af65e8f106f05a98e
Petri Jahnsson <meikyo72@gmail.com>
2018-02-15 18:51:15 +0100

    Add files via upload

--- ne-app/js/main.js
+++ ne-app/js/main.js
@@ -1,5 +1,5 @@
 //window.onload = function () {
-/*let objectPrensentation = document.getElementById('object-presentation');
+let objectPrensentation = document.getElementById('object-presentation');
 let modalDiv = document.getElementById('modalDiv');
 let db = firebase.database();
 let bccRef = db.ref('bcc/');
@@ -9,15 +9,15 @@ bccRef.on("child_added", function(snap, prevChildKey) {
   makeDivFunc(snap.val());
   snap.forEach(function(childSnapshot) {
     let key = childSnapshot.key;
-    let childData = childSnapshot.val();
+    var childData = childSnapshot.val();
     //console.log(key +' '+ childData);
   });
-});*/
+});
 class RealObject extends React.Component {
   render() {
     return (
       <div>
-        <p>Testar lite</p>
+        <p>`${childData}`</p>
       </div>
     )
   }
