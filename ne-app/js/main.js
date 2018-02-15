8154e6d0a72f431d59841ed411c7cd4e420321de
Petri Jahnsson <meikyo72@gmail.com>
2018-02-15 18:56:36 +0100

    Add files via upload

--- ne-app/js/main.js
+++ ne-app/js/main.js
@@ -16,8 +16,16 @@ bccRef.on("child_added", function(snap, prevChildKey) {
 class RealObject extends React.Component {
   render() {
     return (
-      <div>
-        <p>`${childData}`</p>
+      <div class="col-md-4 col-sm-4 col-xs-6 filtr-item" data-category="1" data-sort="Busy streets">
+        <div class="agileits-img">
+          <a href="images/g1.jpg" class="swipebox" title="Lorem ipsum dolor sit amet, consectetur adipiscing elit.">
+            <img class="img-responsive " src="images/g1.jpg" alt=""  />
+            <div class="wthree-pcatn">
+              <h4>Nielsen Estate</h4>
+            </div>
+          </a>
+        </div>
+        <button><a href="#myModal" data-toggle="modal">Detaljerad Information</a></button>
       </div>
     )
   }
