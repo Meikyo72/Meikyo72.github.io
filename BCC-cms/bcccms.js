dad229d6beeb381f2184525a8f4dd626e4245f63
Petri Jahnsson <meikyo72@gmail.com>
2018-05-06 13:31:35 +0200

    sh

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -147,12 +147,11 @@ window.onload = function () {
           let conF = confirm("Vill du radera detta objekt?");
           if (conF == true) {
           firebase.database().ref('bcc/' + key).remove();
-            then(() => tableBody.innerHTML='';
+          tableBody.innerHTML='';
             firebase.database().ref('bcc/').on('child_added', function(snapshot, prevChildKey) {
         				let data = snapshot.val();
         				addMessToTable(data);
         			});
-            )
         } else {
 
         }
