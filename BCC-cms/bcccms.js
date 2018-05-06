c7d3f170b84d9b13c40254cbd287d9319dda0e17
Petri Jahnsson <meikyo72@gmail.com>
2018-05-06 14:03:10 +0200

    failed build fix

--- BCC-cms/bcccms.js
+++ BCC-cms/bcccms.js
@@ -147,11 +147,6 @@ window.onload = function () {
           let conF = confirm("Vill du radera detta objekt?");
           if (conF == true) {
           firebase.database().ref('bcc/' + key).remove();
-          tableBody.innerHTML='';
-          firebase.database().ref('bcc/').on('child_added', function(snapshot, prevChildKey) {
-        		let data = snapshot.val();
-        		addMessToTable(data);
-        			});
         } else {
 
         }
