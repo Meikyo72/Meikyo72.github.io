db0594d61ab111556a22b6082eac6173504868a9
Petri Jahnsson <meikyo72@gmail.com>
2019-01-07 14:36:20 +0100

    Add files via upload

---
+++ Zen/precache-manifest.da16ce68df6f05d2386e07d3b3efcbbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99dd84dec3ee83d62701",
+    "url": "/static/js/main.99dd84de.chunk.js"
+  },
+  {
+    "revision": "620e791fd46c69cfd40d",
+    "url": "/static/js/1.620e791f.chunk.js"
+  },
+  {
+    "revision": "99dd84dec3ee83d62701",
+    "url": "/static/css/main.a98d82d4.chunk.css"
+  },
+  {
+    "revision": "10675f56747dc05498494fc6588eb953",
+    "url": "/index.html"
+  }
+];
